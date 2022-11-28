e07609027dfb39ed50a68212614c0f3adf45a36f
wrajvi <wrajeev13@gmail.com>
2022-11-28 05:48:50 +0530

    Image Added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,7 @@ import styled from "styled-components";
 import CityComponent from "./modules/CityComponent";
 import WeatherComponent from "./modules/WeatherInfoComponent";
 
-export const WeatherIcons = {
-  "01d": "icons/sunny.svg",
-  "01n": "icons/night.svg",
-  "02d": "icons/day.svg",
-  "02n": "icons/cloudy-night.svg",
-  "03d": "icons/cloudy.svg",
-  "03n": "icons/cloudy.svg",
-  "04d": "icons/perfect-day.svg",
-  "04n": "icons/cloudy-night.svg",
-  "09d": "icons/rain.svg",
-  "09n": "icons/rain-night.svg",
-  "10d": "icons/rain.svg",
-  "10n": "icons/rain-night.svg",
-  "11d": "icons/storm.svg",
-  "11n": "icons/storm.svg",
-};
+
 const API_KEY ="eb9940c5a14f25b4d665e30602fa24c7";
 const Container = styled.div`
   display: flex;
--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
-import {WeatherIcons} from "../App";
 
+
+export const WeatherIcons = {
+   "01d": "./icons/sunny.svg",
+   "01n": "./icons/night.svg",
+   "02d": "./icons/day.svg",
+   "02n": "./icons/cloudy-night.svg",
+   "03d": "./icons/cloudy.svg",
+   "03n": "./icons/cloudy.svg",
+   "04d": "./icons/perfect-day.svg",
+   "04n": "./icons/cloudy-night.svg",
+   "09d": "./icons/rain.svg",
+   "09n": "./icons/rain-night.svg",
+   "10d": "./icons/rain.svg",
+   "10n": "./icons/rain-night.svg",
+   "11d": "./icons/storm.svg",
+   "11n": "./icons/storm.svg",
+ };
 export const WeatherInfoIcons = {
-    sunset: "icons/temp.svg",
-    sunrise: "icons/temp.svg",
-    humidity: "icons/humidity.svg",
-    wind: "icons/wind.svg",
-    pressure: "icons/pressure.svg",
+    sunset: "./icons/temp.svg",
+    sunrise: "./icons/temp.svg",
+    humidity: "./icons/humidity.svg",
+    wind: "./icons/wind.svg",
+    pressure: "./icons/pressure.svg",
 };
 const Location = styled.span`
   margin: 15px auto;
